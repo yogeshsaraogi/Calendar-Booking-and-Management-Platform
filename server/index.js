d6d17e2473e9b8f1186737a46963a8f1d3be3973
Yogesh <yogeshsarowgi@gmail.com>
2023-12-14 14:45:15 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require("express");
+const app = express();
+const port = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
