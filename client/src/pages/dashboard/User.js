f54215c6eba0b1600981fbe0a0bd7d7cd2424c22
Yogesh <yogeshsarowgi@gmail.com>
2023-12-18 02:24:57 +0530

    fix

--- client/src/pages/dashboard/User.js
+++ client/src/pages/dashboard/User.js
@@ -75,7 +75,7 @@ const User = () => {
       setCategories(res?.categories);
     });
     fetchUserScheduled();
-  });
+  }, []);
 
   useEffect(() => {
     fetchGetTimeSlot();
--- client/src/url.js
+++ client/src/url.js
@@ -1,4 +1,5 @@
-const baseUrl = "http://localhost:3001";
+// const baseUrl = "http://localhost:3001";
+const baseUrl = "https://calender-management-server.onrender.com";
 
 export const API_URLS = {
   LOGIN: `${baseUrl}/auth/login`,
